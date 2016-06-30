7a65bd3e07c1562cfa02b7735b43d34edd5bc038
manmanking <ws@idontknow.local>
2016-06-30 13:19:36 +0800

    创建工程

---
+++ helloword/helloword/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  helloword
+//
+//  Created by ws on 16/6/30.
+//  Copyright © 2016年 cad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
